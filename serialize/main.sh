da207461e818509b4531cd176f839e2031421065
David Kamholz <lautgesetz@gmail.com>
2013-01-23 15:47:45 -0800

    lookup path to perl in main.sh rather than hard-coding it

--- serialize/main.sh
+++ serialize/main.sh
@@ -1,6 +1,9 @@
 # Converts a tab-delimited approver file for uploading to PanLex.
 
-/usr/bin/perl -C63 -w apostrophe.pl 'aaa-bbb-Author' 0 '1:gyd-000' '3:nny-000' '5:eng-000'
+# Find the path to the perl executable.
+PERL=`which perl`
+
+$PERL -C63 -w apostrophe.pl 'aaa-bbb-Author' 0 '1:gyd-000' '3:nny-000' '5:eng-000'
 # Converts a tab-delimited approver file’s apostrophes.
 # Arguments:
 #	0: base of the filename.
@@ -8,7 +11,7 @@
 #	2+: specifications (column index and variety UID, colon-delimited) of columns
 #		possibly requiring apostrophe normalization.
 
-/usr/bin/perl -C63 -w extag.pl 'aaa-bbb-Author' 1 '‣' '⁋' '⫷ex⫸' '⫷mn⫸' 0 2
+$PERL -C63 -w extag.pl 'aaa-bbb-Author' 1 '‣' '⁋' '⫷ex⫸' '⫷mn⫸' 0 2
 # Tags all expressions and all intra-column meaning changes in a tab-delimited approver file,
 # disregarding any definitional parts.
 # Arguments:
@@ -20,7 +23,7 @@
 #	5: meaning tag.
 #	6+: columns containing expressions.
 
-/usr/bin/perl -C63 -w exdftag.pl 'aaa-bbb-Author' 2 '⫷ex⫸' '[^⫷]' '[^⫷ ]' '(?:\([^()]+\)|（[^（）]+）)' '⫷df⫸' 25 3 '[][/,;?!~]' '«[^«»]+»' 2
+$PERL -C63 -w exdftag.pl 'aaa-bbb-Author' 2 '⫷ex⫸' '[^⫷]' '[^⫷ ]' '(?:\([^()]+\)|（[^（）]+）)' '⫷df⫸' 25 3 '[][/,;?!~]' '«[^«»]+»' 2
 # Splits definitional expressions into reduced expressions and definitions in an approver file with
 # already-tagged expressions and tags the added definitions.
 # Arguments:
@@ -39,7 +42,7 @@
 #		or blank if none.
 #	11+: columns containing expressions that may contain definitional parts.
 
-/usr/bin/perl -C63 -w dftag.pl 'aaa-bbb-Author' 3 '⫷df⫸' 1 2
+$PERL -C63 -w dftag.pl 'aaa-bbb-Author' 3 '⫷df⫸' 1 2
 # Tags all column-based definitions in a tab-delimited approver file.
 # Arguments:
 #	0: base of the filename.
@@ -47,7 +50,7 @@
 #	2: definition tag.
 #	3+: columns containing definitions.
 
-/usr/bin/perl -C63 -w mitag.pl 'aaa-bbb-Author' 4 2 '⫷mi⫸'
+$PERL -C63 -w mitag.pl 'aaa-bbb-Author' 4 2 '⫷mi⫸'
 # Tags meaning identifiers.
 # Arguments:
 #	0: base of the filename.
@@ -55,7 +58,7 @@
 #	2: column that contains meaning identifiers.
 #	3: meaning-identifier tag.
 
-/usr/bin/perl -C63 -w wcretag.pl 'aaa-bbb-Author' 2 '⫷wc:' '⫸' '⫷wc⫸' '⫷md:gram⫸' 1 2
+$PERL -C63 -w wcretag.pl 'aaa-bbb-Author' 2 '⫷wc:' '⫸' '⫷wc⫸' '⫷md:gram⫸' 1 2
 # Retags word classifications in a tab-delimited approver file.
 # Arguments:
 #	0: base of the filename.
@@ -66,7 +69,7 @@
 #	5: metadatum tag.
 #	6+: columns containing word classifications.
 
-/usr/bin/perl -C63 -w wctag.pl 'aaa-bbb-Author' 5 1 '⫷wc⫸' '⫷md:gram⫸'
+$PERL -C63 -w wctag.pl 'aaa-bbb-Author' 5 1 '⫷wc⫸' '⫷md:gram⫸'
 # Converts and tags word classifications in a tab-delimited approver file.
 # Arguments:
 #	0: base of the filename.
@@ -75,7 +78,7 @@
 #	3: word-classification tag.
 #	4: metadatum tag.
 
-/usr/bin/perl -C63 -w mdtag.pl 'aaa-bbb-Author' 6 2 '⫷md:gram⫸'
+$PERL -C63 -w mdtag.pl 'aaa-bbb-Author' 6 2 '⫷md:gram⫸'
 # Tags metadata in a tab-delimited approver file.
 # Arguments:
 #	0: base of the filename.
@@ -83,7 +86,7 @@
 #	2: column containing metadata.
 #	3: metadatum tag.
 
-/usr/bin/perl -C63 -w dmtag.pl 'aaa-bbb-Author' 7 '⫷dm⫸' '‣' 2 3
+$PERL -C63 -w dmtag.pl 'aaa-bbb-Author' 7 '⫷dm⫸' '‣' 2 3
 # Tags domain expressions in a tab-delimited approver file.
 # Arguments:
 #	0: base of the filename.
@@ -92,7 +95,7 @@
 #	3: inter-expression delimiter, or blank if none.
 #	4+: columns containing domain expressions.
 
-/usr/bin/perl -C63 -w mnsplit.pl 'aaa-bbb-Author' 8 '⫷mn⫸' 2
+$PERL -C63 -w mnsplit.pl 'aaa-bbb-Author' 8 '⫷mn⫸' 2
 # Splits multi-meaning lines of a tagged approver file, eliminating any duplicate output lines.
 # Arguments:
 #	0: base of the filename.
@@ -100,7 +103,7 @@
 #	2: meaning-delimitation tag.
 #	3: number (0-based) of the column that may contain multiple meanings.
 
-/usr/bin/perl -C63 -w wcshift.pl 'aaa-bbb-Author' 9 2 '«wc:' '»' '⫷wc⫸' '⫷ex⫸' '[^⫷]'
+$PERL -C63 -w wcshift.pl 'aaa-bbb-Author' 9 2 '«wc:' '»' '⫷wc⫸' '⫷ex⫸' '[^⫷]'
 # Replaces prepended word class specifications with post-ex wc tags in a
 # tab-delimited approver file.
 # Arguments:
@@ -113,7 +116,7 @@
 #	6: expression tag.
 #	7: regular expression matching any post-tag character.
 
-/usr/bin/perl -C63 -w normalize.pl 'aaa-bbb-Author' 10 '⫷[a-z:]+⫸' '⫷ex⫸' 0 50 10 'eng-000' '⫷exp⫸' '⫷df⫸' ', '
+$PERL -C63 -w normalize.pl 'aaa-bbb-Author' 10 '⫷[a-z:]+⫸' '⫷ex⫸' 0 50 10 'eng-000' '⫷exp⫸' '⫷df⫸' ', '
 # Normalizes expressions in a tagged approver file.
 # Arguments:
 #	0: base of the filename.
@@ -137,7 +140,7 @@
 #		they are to be normalized if and only if all expressions in the list are
 #		normalizable, or blank if not.
 
-/usr/bin/perl -C63 -w out-simple-0.pl 'aaa-bbb-Author' 11 'final' '0:epo-000' '1:hun-000'
+$PERL -C63 -w out-simple-0.pl 'aaa-bbb-Author' 11 'final' '0:epo-000' '1:hun-000'
 # Converts a normally tagged approver file to a simple-text varilingual approver file,
 # eliminating duplicates.
 # Arguments:
@@ -147,7 +150,7 @@
 #	3+: specifications (column index and variety UID, colon-delimited) of columns
 #		containing expressions.
 
-/usr/bin/perl -C63 -w out-simple-2.pl 'aaa-bbb-Author' 12 'final' 'rus-000' 'eng-000'
+$PERL -C63 -w out-simple-2.pl 'aaa-bbb-Author' 12 'final' 'rus-000' 'eng-000'
 # Converts a normally tagged approver file to a simple-text bilingual approver file,
 # eliminating duplicates.
 # Arguments:
@@ -157,7 +160,7 @@
 #	3: variety UID of column 0.
 #	4: variety UID of column 1.
 
-/usr/bin/perl -C63 -w out-full-0.pl 'aaa-bbb-Author' 12 'final' '' 2 2 '0:eng-000' '1:haa-000'
+$PERL -C63 -w out-full-0.pl 'aaa-bbb-Author' 12 'final' '' 2 2 '0:eng-000' '1:haa-000'
 # Converts a standard tagged approver file to a full-text varilingual approver file.
 # Arguments:
 #	0: base of the filename.
@@ -170,7 +173,7 @@
 #	6+: specifications (column index and variety UID, colon-delimited) of columns
 #		containing tags (ex, df, dm) requiring variety specifications.
 
-/usr/bin/perl -C63 -w out-full-2.pl 'aaa-bbb-Author' 12 'final' '' 2 1 '0:art-259' '2:eng-000'
+$PERL -C63 -w out-full-2.pl 'aaa-bbb-Author' 12 'final' '' 2 1 '0:art-259' '2:eng-000'
 # Converts a standard tagged approver file to a full-text bilingual approver file, eliminating duplicates.
 # Arguments:
 #	0: base of the filename.
