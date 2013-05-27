51ed0865935d15c84a50a5d4fcebbd77812a4088
David Kamholz <lautgesetz@gmail.com>
2013-05-27 13:47:10 -0700

    clean up out-full-0.pl

--- serialize/out-full-0.pl
+++ serialize/out-full-0.pl
@@ -26,108 +26,106 @@ $PanLex::Serialize::out_full_0::final = 1;
 
 sub process {
 
-	my ($in, $out, $wc, $mindf, $minex, @spec) = @_;
+    my ($in, $out, $wc, $mindf, $minex, @spec) = @_;
 
-	die "invalid minimum count\n" if ($mindf < 2) || ($minex < 1);
-	# If either minimum count is too small, quit and notify the user.
+    die "invalid minimum count\n" if ($mindf < 2) || ($minex < 1);
+    # If either minimum count is too small, quit and notify the user.
 
-	print $out ":\n0\n";
-	# Output the file header.
+    print $out ":\n0\n";
+    # Output the file header.
 
-	my (%col, %en);
+    my (%col, %en);
 
-	foreach my $i (@spec) {
-	# For each variety-specific column:
+    foreach my $i (@spec) {
+    # For each variety-specific column:
 
-		my @col = split /:/, $i;
-		# Identify its specification parts.
+        my @col = split /:/, $i;
+        # Identify its specification parts.
 
-		validate_spec(@col);
+        validate_spec(@col);
 
-		$col{$col[0]} = $col[1];
-		# Add its index and variety UID to the table of variety-specific columns.
+        $col{$col[0]} = $col[1];
+        # Add its index and variety UID to the table of variety-specific columns.
 
-	}
+    }
 
-	while (<$in>) {
-	# For each line of the input file:
+    while (<$in>) {
+    # For each line of the input file:
 
-		chomp;
-		# Delete its trailing newline.
+        chomp;
+        # Delete its trailing newline.
 
-		my @col = split /\t/, $_, -1;
-		# Identify its columns.
+        my @col = split /\t/, $_, -1;
+        # Identify its columns.
 
-		for (my $i = 0; $i < @col; $i++) {
-		# For each of them:
+        for (my $i = 0; $i < @col; $i++) {
+        # For each of them:
 
-			if (exists $col{$i}) {
-			# If it is variety-specific:
+            if (exists $col{$i}) {
+            # If it is variety-specific:
 
-				$col[$i] =~ s/⫷ex⫸/⫷ex:$col{$i}⫸/g;
-				# Insert the column's variety UID into each expression tag in it.
+                $col[$i] =~ s/⫷ex⫸/⫷ex:$col{$i}⫸/g;
+                # Insert the column's variety UID into each expression tag in it.
 
-				$col[$i] =~ s/⫷df⫸/⫷df:$col{$i}⫸/g;
-				# Insert the column's variety UID into each definition tag in it.
+                $col[$i] =~ s/⫷df⫸/⫷df:$col{$i}⫸/g;
+                # Insert the column's variety UID into each definition tag in it.
 
-				$col[$i] =~ s/⫷dm⫸/⫷dm:$col{$i}⫸/g;
-				# Insert the column's variety UID into each domain tag in it.
+                $col[$i] =~ s/⫷dm⫸/⫷dm:$col{$i}⫸/g;
+                # Insert the column's variety UID into each domain tag in it.
 
-			}
+            }
 
-		}
+        }
 
-		my $en = join '', @col;
-		# Identify a concatenation of its modified columns.
+        my $en = join '', @col;
+        # Identify a concatenation of its modified columns.
 
-		$en =~ s/⫷exp⫸.+?(?=⫷ex:|⫷df|⫷dm|⫷mi|$)//g;
-		# Delete all deprecated (i.e. pre-normalized) expressions in it.
+        $en =~ s/⫷exp⫸.+?(?=⫷ex:|⫷df|⫷dm|⫷mi|$)//g;
+        # Delete all deprecated (i.e. pre-normalized) expressions in it.
 
-		while ($en =~ s/(⫷df:[a-z]{3}-\d{3}⫸[^⫷]+)⫷(?:wc|md:[^⫸]+)⫸[^⫷]+/$1/) {}
-		# Delete all word classifications or metadata following definitions.
+        while ($en =~ s/(⫷df:[a-z]{3}-\d{3}⫸[^⫷]+)⫷(?:wc|md:[^⫸]+)⫸[^⫷]+/$1/) {}
+        # Delete all word classifications or metadata following definitions.
 
-		while ($en =~ s/((⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)(?:⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)*)\2(?=⫷|$)/$1/) {}
-		# Delete all duplicate wc and md elements of any ex element in it.
+        while ($en =~ s/((⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)(?:⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)*)\2(?=⫷|$)/$1/) {}
+        # Delete all duplicate wc and md elements of any ex element in it.
 
-		while ($en =~ s/((⫷((?:df|dm):[^⫸]+)⫸[^⫷⫸]+)(?:⫷.+?)?)\2(?=⫷|$)/$1/) {}
-		# Delete all duplicate df and dm elements in it.
+        while ($en =~ s/((⫷((?:df|dm):[^⫸]+)⫸[^⫷⫸]+)(?:⫷.+?)?)\2(?=⫷|$)/$1/) {}
+        # Delete all duplicate df and dm elements in it.
 
-		while ($en =~ s/((⫷ex:[^⫸]+⫸[^⫷⫸]+)(?:⫷.+?)?)\K\2(?:⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)*(?=⫷|$)//) {}
-		# Delete all duplicate ex elements in it.
+        while ($en =~ s/((⫷ex:[^⫸]+⫸[^⫷⫸]+)(?:⫷.+?)?)\K\2(?:⫷(?:wc|md:[^⫸]+)⫸[^⫷⫸]+)*(?=⫷|$)//) {}
+        # Delete all duplicate ex elements in it.
 
-		my @exdf;
+        next if (
+            (() = $en =~ /(⫷(?:ex|df):)/g) < $mindf
+            || (() = $en =~ /(⫷ex:)/g) < $minex
+        );
+        # If the count of remaining expressions and definitions or the count of remaining
+        # expressions is smaller than the minimum, disregard the line.
 
-		next if (
-			((@exdf = ($en =~ /(⫷(?:ex|df):)/g)) < $mindf)
-			|| ((@exdf = ($en =~ /(⫷ex:)/g)) < $minex)
-		);
-		# If the count of remaining expressions and definitions or the count of remaining
-		# expressions is smaller than the minimum, disregard the line.
+        $en =~ s/⫷mi⫸/\nmi\n/g;
+        # Convert all meaning-identifier tags in it.
 
-		$en =~ s/⫷mi⫸/\nmi\n/g;
-		# Convert all meaning-identifier tags in it.
+        $en =~ s/⫷(ex|df|dm):([a-z]{3}-\d{3})⫸/\n$1\n$2\n/g;
+        # Convert all expression, definition, and domain tags in it.
 
-		$en =~ s/⫷(ex|df|dm):([a-z]{3}-\d{3})⫸/\n$1\n$2\n/g;
-		# Convert all expression, definition, and domain tags in it.
+        $en =~ s/⫷wc⫸/\nwc\n/g;
+        # Convert all word-classification tags in it.
 
-		$en =~ s/⫷wc⫸/\nwc\n/g;
-		# Convert all word-classification tags in it.
+        $en =~ s/⫷md:(.+?)⫸/\nmd\n$1\n/g;
+        # Convert all metadatum tags in it.
 
-		$en =~ s/⫷md:(.+?)⫸/\nmd\n$1\n/g;
-		# Convert all metadatum tags in it.
+        unless (exists $en{$en}) {
+        # If the converted line is not a duplicate:
 
-		unless (exists $en{$en}) {
-		# If the converted line is not a duplicate:
+            $en{$en} = '';
+            # Add it to the table of lines.
 
-			$en{$en} = '';
-			# Add it to the table of lines.
+            print $out $en, "\n";
+            # Output it.
 
-			print $out $en, "\n";
-			# Output it.
+        }
 
-		}
-
-	}
+    }
 
 }
 
