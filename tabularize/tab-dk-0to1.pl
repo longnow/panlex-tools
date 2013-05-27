41d68e4ae0ae2c0d1c46c50570e5935b7e212d10
David Kamholz <lautgesetz@gmail.com>
2013-05-27 13:46:46 -0700

    clean up tab-dk-0to1.pl

--- tabularize/tab-dk-0to1.pl
+++ tabularize/tab-dk-0to1.pl
@@ -26,31 +26,29 @@ open my $in, '<:bytes', "$BASENAME-$VERSION.dk";
 open my $out, '>:encoding(utf8)', ("$BASENAME-" . ($VERSION + 1) . '.txt');
 # Create or truncate the output file and open it for writing.
 
-my $ret = (join '', <$in>);
+my $ret = join('', <$in>);
 # Initialize the result as a concatenation of all lines in the input file.
 
-$ret = (substr $ret, 106);
+$ret = substr($ret, 106);
 # Delete the first 106 characters from the result.
 
 my @seg = ($ret =~ m/((?:.|\n){40})/g);
 # Split the result into 40-character segments.
 
-my ($seg, @trio);
-
-foreach $seg (@seg) {
+foreach my $seg (@seg) {
 # For each of them:
 
-	@trio = ((substr $seg, 0, 1), (substr $seg, 1, 31), (substr $seg, 31, 4));
-	# Identify its length, lemma, and index.
+    my @trio = (substr($seg, 0, 1), substr($seg, 1, 31), substr($seg, 31, 4));
+    # Identify its length, lemma, and index.
 
-	$trio[1] = (substr $trio[1], 0, (unpack 'C', $trio[0]));
-	# Shorten the lemma to its specified length.
+    $trio[1] = substr($trio[1], 0, unpack('C', $trio[0]));
+    # Shorten the lemma to its specified length.
 
-	$trio[2] = (unpack 'N', $trio[2]);
-	# Convert the index from the 32-bit string to a value.
+    $trio[2] = unpack('N', $trio[2]);
+    # Convert the index from the 32-bit string to a value.
 
-	print $out ((join "\t", @trio[2, 1]), "\n");
-	# Output the lemma and index, tab-delimited.
+    print $out join("\t", @trio[2, 1]), "\n";
+    # Output the lemma and index, tab-delimited.
 
 }
 
