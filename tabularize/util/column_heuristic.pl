5e7712f435e0e922ea1c00fc19c23c6a07158678
David Kamholz <lautgesetz@gmail.com>
2013-07-17 16:02:36 -0700

    more column_heuristic improvements

--- tabularize/util/column_heuristic.pl
+++ tabularize/util/column_heuristic.pl
@@ -1,25 +1,32 @@
 use strict;
 use utf8;
-use List::Util qw/max min sum/;
+use List::Util qw/max min/;
+use Statistics::Basic qw/median/;
 
 # the number of positions to look (left and right) in each
 # successive iteration while refining the heuristic.
 my $LOOK_RANGE = 10;
 
+# the minimum width of a column. candidate heuristics with columns
+# smaller than this will be discarded.
+my $MIN_WIDTH = 5;
+
 # returns the cumulative score for position $pos in $lines.
 # by default, gives a score of 1 for each space. modify as needed.
 sub score_pos {
-    my ($lines, $pos, $maxlen) = @_;
+    my ($lines, $pos, $maxwidth) = @_;
     
     my $score = 0;
     
-    foreach my $line (@$lines) {
-        next if substr($line, 0, $pos) =~ /^\s+$/;
-        next if substr($line, $pos+1) =~ /^\s+$/;
-        
+    foreach my $line (@$lines) {        
         if (substr($line,$pos,1) eq ' ') {
+            # ignore cases where there is nothing but whitespace to one side, as this
+            # is not informative for the location of a column boundary.
+            next if substr($line, 0, $pos) =~ /^\s+$/;
+            next if substr($line, $pos+1) =~ /^\s+$/;
+
             $score++;
-            #$score += 5 * count_adjacent_spaces($line, $pos, $maxlen) / $maxlen;
+            #$score += 5 * count_adjacent_spaces($line, $pos, $maxwidth) / $maxwidth;
         }
     }
     
@@ -29,17 +36,17 @@ sub score_pos {
 }
 
 sub count_adjacent_spaces {
-    my ($line, $pos, $maxlen) = @_;
+    my ($line, $pos, $maxwidth) = @_;
     my $count = 0;
     
     for (my $i = $pos - 1; $i >= 0 && substr($line,$i,1) eq ' '; $i--) {
         $count++;
-    }
-
-    for (my $i = $pos + 1; $i < $maxlen && substr($line,$i,1) eq ' '; $i++) {
+    }        
+    
+    for (my $i = $pos + 1; $i < $maxwidth && substr($line,$i,1) eq ' '; $i++) {
         $count++;
     }        
-
+    
     return $count;
 }
 
@@ -53,25 +60,36 @@ sub column_heuristic {
     $lines = [@$lines];
     
     my @len = map { length } @$lines;
-    my $maxlen = max(@len);
-    my $avglen = sum(@len) / @len;
+    my $maxwidth = max(@len);
     
     foreach my $line (@$lines) {
-        $line .= ' ' x ($maxlen - length $line) if length $line < $maxlen;
+        $line .= ' ' x ($maxwidth - length $line) if length $line < $maxwidth;
     }
     
+    my @candidates = 
+        grep { valid_candidate($_) }
+        map { _column_heuristic($num, $lines, $maxwidth, $_) } (median(@len), $maxwidth);
+    
+    die "could not find any candidate heuristics" unless @candidates;
+    
+    return [0, @{$candidates[0]} ];
+}
+
+sub _column_heuristic {
+    my ($num, $lines, $maxwidth, $linewidth) = @_;
+
     my $h = [];
     
-    my $start_width = int($avglen / $num);
+    my $startwidth = int($linewidth / $num);
     for my $i (1 .. $num - 1) {
-        push @$h, $start_width * $i;        
+        push @$h, $startwidth * $i;        
     }
 
     my %seen;
     $seen{hash_heuristic($h)}++;
         
     while (1) {
-        my $newh = refine_heuristic($h, $lines, $maxlen);
+        my $newh = refine_heuristic($h, $lines, $maxwidth);
                 
         my $diff = 0;
         for (my $i = 0; $i < $num; $i++) {
@@ -85,7 +103,46 @@ sub column_heuristic {
         last if $diff == 0 || $seen{$hash} > 5;
     }
     
-    return [0, @$h];
+    return $h;
+}
+
+sub refine_heuristic {
+    my ($h, $lines, $maxwidth) = @_;
+    $h = [@$h];
+    
+    foreach my $pos (@$h) {
+        my @try_pos = ($pos);
+        for my $i (1 .. $LOOK_RANGE) {
+            push @try_pos, $pos - $i, $pos + $i;            
+        }
+        @try_pos = grep { $_ >= 0 && $_ < $maxwidth } @try_pos;
+        
+        my %score = map { $_ => score_pos($lines, $_, $maxwidth) } @try_pos;
+        my $maxscore = max(values %score);
+        my @candidates = grep { $score{$_} == $maxscore } @try_pos;
+        my %distance = map { abs($_ - $pos) => $_ } @candidates;
+        
+        $pos = $distance{min(keys %distance)};
+    }
+    
+    return $h;
+}
+
+sub hash_heuristic {
+    my ($h) = @_;
+    return join('|', map { sprintf "%04d", $_ } @$h);
+}
+
+sub valid_candidate {
+    my ($h) = @_;
+    
+    return 0 if $h->[0] < $MIN_WIDTH;
+    
+    for (my $i = 1; $i < @$h; $i++) {
+        return 0 if $h->[$i] - $h->[$i-1] < $MIN_WIDTH;
+    }
+    
+    return 1;
 }
 
 # parses the columns from a line using a generated heuristic.
@@ -103,33 +160,6 @@ sub heuristic_parse {
     push @rec, substr($line, $h->[-1]);
     
     return @rec;
-}
-
-sub refine_heuristic {
-    my ($h, $lines, $maxlen) = @_;
-    $h = [@$h];
-    
-    foreach my $pos (@$h) {
-        my @try_pos = ($pos);
-        for my $i (1 .. $LOOK_RANGE) {
-            push @try_pos, $pos - $i, $pos + $i;            
-        }
-        @try_pos = grep { $_ >= 0 && $_ < $maxlen } @try_pos;
-        
-        my %score = map { $_ => score_pos($lines, $_, $maxlen) } @try_pos;
-        my $maxscore = max(values %score);
-        my @candidates = grep { $score{$_} == $maxscore } @try_pos;
-        my %distance = map { abs($_ - $pos) => $_ } @candidates;
-        
-        $pos = $distance{min(keys %distance)};
-    }
-    
-    return $h;
-}
-
-sub hash_heuristic {
-    my ($h) = @_;
-    return join('|', map { sprintf "%04d", $_ } @$h);
 }
 
 1;
