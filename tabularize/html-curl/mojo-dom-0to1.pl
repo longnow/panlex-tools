5738c25e7c3d73358a027670c04ed6ee66da6a66
David Kamholz <lautgesetz@gmail.com>
2013-07-25 10:38:04 -0700

    add Mojo::DOM tabularize template

---
+++ tabularize/html-curl/mojo-dom-0to1.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+
+use warnings 'FATAL', 'all';
+# Make every warning fatal.
+
+use strict;
+# Require strict checking of variable references, etc.
+
+use utf8;
+# Make Perl interpret the script as UTF-8.
+
+binmode STDOUT, ':encoding(utf8)';
+binmode STDERR, ':encoding(utf8)';
+# make STDOUT and STDERR print in UTF-8.
+
+use Mojo::DOM;
+
+#######################################################
+
+my $BASENAME = 'aaa-bbb-Author';
+# Identify the filename base.
+
+my $VERSION = 0;
+# Identify the input file's version.
+
+#######################################################
+
+open my $in, '<:encoding(utf8)', "$BASENAME-$VERSION.html" or die $!;
+# Open the input file for reading.
+
+open my $out, '>:encoding(utf8)', ("$BASENAME-" . ($VERSION + 1) . '.txt') or die $!;
+# Create or truncate the output file and open it for writing.
+
+my $html = do { local $/; <$in> };
+# Read in the whole HTML input.
+
+my $dom = Mojo::DOM->new($html);
+# Create a Mojo::DOM object to query the HTML document.
+
+close $in;
+# Close the input file.
+
+close $out;
+# Close the output file.
