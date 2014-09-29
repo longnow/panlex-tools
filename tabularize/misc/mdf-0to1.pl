642d7ab732fce21cbd4f18720311f2a43efae63d
David Kamholz <lautgesetz@gmail.com>
2014-09-29 13:30:38 -0700

    add basic mdf parsing script

---
+++ tabularize/misc/mdf-0to1.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+
+use warnings 'FATAL', 'all';
+# Make every warning fatal.
+
+use strict;
+# Require strict checking of variable references, etc.
+
+use utf8;
+# Make Perl interpret the script as UTF-8.
+
+binmode STDOUT, ':encoding(utf8)';
+binmode STDERR, ':encoding(utf8)';
+# make STDOUT and STDERR print in UTF-8.
+
+use lib "$ENV{PANLEX_TOOLDIR}/lib";
+use PanLex::Util;
+
+#######################################################
+
+my $BASENAME = 'aaa-bbb-Author';
+# Identify the filename base.
+
+my $VERSION = 0;
+# Identify the input file's version.
+
+#######################################################
+
+open my $out, '>:encoding(utf8)', ("$BASENAME-" . ($VERSION + 1) . '.txt') or die $!;
+# Create or truncate the output file and open it for writing.
+
+open my $in, '<:encoding(utf8)', "$BASENAME-$VERSION.txt" or die $!;
+# Open the input file for reading.
+
+# skip over the MDF header.
+while (<$in>) {
+    last if /^\s*$/;
+}
+
+my %rec;
+
+while (<$in>) {
+# For each line of the input file:
+
+    chomp;
+    # remove its trailing newline, if present.
+
+    if (/^\\([a-z]+) +(.+)$/) {
+        handle_marker($1, $2);
+    }
+    elsif (/^\s*$/) {
+        output_line();
+        %rec = ();
+    }
+}
+
+close $in;
+# Close the input file.
+
+close $out;
+# Close the output file.
+
+# called for every marker.
+sub handle_marker {
+    my ($marker, $txt) = @_;
+
+    if ($marker =~ /^(lx|ps|g[ern])$/) {
+        $rec{$marker} = Trim($txt);
+    }
+    elsif ($marker eq 'sn' && $txt > 1) {
+        output_line();
+        $rec{$_} = '' for qw(ps ge gr gn);
+    }
+}
+
+# outputs a single line. called at the end of every record and can also be called within records.
+sub output_line {
+    print $out join("\t", map { $rec{$_}||'' } qw(lx ps ge)), "\n";
+}
