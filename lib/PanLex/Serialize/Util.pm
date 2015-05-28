c58e4a7d299b246280e370c947806096831571bf
David Kamholz <lautgesetz@gmail.com>
2015-05-28 01:18:35 -0700

    port wctag, remove wcretag/wcshift

--- lib/PanLex/Serialize/Util.pm
+++ lib/PanLex/Serialize/Util.pm
@@ -5,33 +5,7 @@ use utf8;
 use parent 'Exporter';
 use File::Spec::Functions;
 
-our @EXPORT = qw/load_wc parse_specs/;
-
-sub load_wc {
-    my $wctxt = -e 'wc.txt' ? 'wc.txt' : catfile($ENV{PANLEX_TOOLDIR}, 'serialize', 'data', 'wc.txt');
-
-    open my $fh, '<:encoding(utf8)', $wctxt or die $!;
-    # Open the wc file for reading.
-
-    my $wc;
-
-    while (my $line = <$fh>) {
-    # For each line of the wc file:
-
-        chomp $line;
-        # Delete its trailing newline.
-
-        my @col = split /\t/, $line, -1;
-        # Identify its columns.
-
-        $wc->{$col[0]} = [ split /:/, $col[1] ];
-        # Add it to the table of wc conversions.
-    }
-    
-    close $fh;
-
-    return $wc;
-}
+our @EXPORT = qw/parse_specs/;
 
 sub parse_specs {
     my ($specs) = @_;
