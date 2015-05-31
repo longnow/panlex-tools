ee8bca8e2db3429e07e345c0d84f36548832fb0f
David Kamholz <lautgesetz@gmail.com>
2015-05-31 14:55:43 -0700

    fix last commit

--- lib/PanLex/Serialize/csppmap.pm
+++ lib/PanLex/Serialize/csppmap.pm
@@ -14,7 +14,7 @@ package PanLex::Serialize::csppmap;
 use strict;
 use warnings 'FATAL', 'all';
 use utf8;
-use open ':raw:encoding(utf8)';
+use open IO => ':raw:encoding(utf8)';
 use parent 'Exporter';
 use File::Spec::Functions;
 use PanLex::Validation;
