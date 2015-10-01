eb4d6234fb2fd344b2784af7c3d473e393418f06
David Kamholz <lautgesetz@gmail.com>
2015-09-30 17:00:46 -0700

    add parse_tags and deparse_tags methods

--- lib/PanLex/Serialize/Util.pm
+++ lib/PanLex/Serialize/Util.pm
@@ -5,7 +5,7 @@ use utf8;
 use parent 'Exporter';
 use File::Spec::Functions;
 
-our @EXPORT = qw/parse_specs/;
+our @EXPORT = qw/parse_specs parse_tags deparse_tags/;
 
 sub parse_specs {
     my ($specs) = @_;
@@ -18,6 +18,34 @@ sub parse_specs {
     }
 
     return \%col_uid;
+}
+
+sub parse_tags {
+    my ($str) = @_;
+
+    my @tags;
+
+    while ($str =~ /⫷([a-z0-9]+)(?::([a-z]{3}-\d{3}))?⫸([^⫷]+)/g) {
+        push @tags, [ $1, $2, $3 ];
+    }
+
+    return \@tags;
+}
+
+sub deparse_tags {
+    my ($tags) = @_;
+
+    my $str = '';
+
+    foreach my $tag (@$tags) {
+        my ($type, $uid, $content) = @$tag;
+
+        $str .= "⫷$type";
+        $str .= ":$uid" if defined $uid;
+        $str .= "⫸$content";
+    }
+
+    return $str;
 }
 
 1;
