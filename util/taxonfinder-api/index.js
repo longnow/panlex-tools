58cc67842b64f46722c835318e54fa9cf4317911
David Kamholz <lautgesetz@gmail.com>
2015-12-17 16:28:22 -0800

    make taxonfinder-api output message more intelligible

--- util/taxonfinder-api/index.js
+++ util/taxonfinder-api/index.js
@@ -12,5 +12,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);    
+  console.log('taxonfinder-api listening at http://%s:%s', host, port);    
 });
