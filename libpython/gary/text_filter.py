7b87c41f1823d4c5240e6841c54d024abd838d5d
Gary Krug <garykpdx145@gmail.com>
2016-01-25 16:04:30 -0800

    added flatten parentheses filter

--- libpython/gary/text_filter.py
+++ libpython/gary/text_filter.py
@@ -1,6 +1,7 @@
 
 import regex as re
-
+import sys
+import unicodedata
 
 from . import ignore_parens
 
@@ -8,12 +9,28 @@ def strip_ws(text, **kwargs):
     return text.strip()
 
 
+
+def pre_process(text):
+    text = re.sub('\s*\.{3,}\s*', ' … ', text)
+    text = re.sub('\s+', ' ', text)
+    text = unicodedata.normalize('NFC', text)
+
+    return text.strip()
+
+
+message_is_displayed = True
 def normalize_punctuation(text):
-    text = re.sub('[.,/?!]+\s*$', '', text)
+    global message_is_displayed
+    if not message_is_displayed:
+        print('DEPRECATED: use pre_process instead',file=sys.stderr)
+        message_is_displayed = True
+    text = re.sub('[.,/?!;]+\s*$', '', text)
     text = re.sub('\s*\.{3,}\s*', ' … ', text)
     text = re.sub('^\((.*)\)$', r'\1', text)
 
-    return text
+    return text.strip()
+
+
 
 def remove_final_punct(text, **kwargs):
     text = re.sub('[.,/?!]+\s*$', '', text)
@@ -35,3 +52,22 @@ def apply(text, filter_list,debug=False):
     return text
 
 
+def flatten_parentheses(text:str, remove=False) -> str:
+    if remove:
+        replace_start, replace_end = ''
+    else:
+        replace_start, replace_end = ('[',']')
+
+    text = list(text)
+    count = 0
+    for i in range(len(text)):
+        if text[i] == '(':
+            count += 1
+            if count > 1:
+                text[i] = replace_start
+        elif text[i] == ')':
+            if count > 1:
+                text[i] = replace_end
+            count -= 1
+
+    return ''.join(text)
