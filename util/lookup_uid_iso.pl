0ce63cfcac222709fcaa31c9e5d8a14bfc8c00d0
David Kamholz <lautgesetz@gmail.com>
2016-12-28 14:00:43 -0800

    add glottocode support

--- util/lookup_uid_iso.pl
+++ util/lookup_uid_iso.pl
@@ -1,3 +1,4 @@
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use lib "$ENV{PANLEX_TOOLDIR}/lib";
@@ -6,13 +7,14 @@ use List::Util 'uniq';
 binmode STDOUT, ':encoding(utf8)';
 
 my @ISO_UID = qw( art-001 art-002 art-003 art-005 art-015 );
+my $GLOTTO_UID = 'art-327';
 
 die "you must provide a filename (one language name per line)" unless @ARGV;
 
 my ($filename) = @ARGV;
 die "could not read file: $filename" unless -r $filename;
 
-my (@langnames, %langname_td, %td_uid, %td_iso);
+my (@langnames, %langname_td, %td);
 
 open my $fh, '<:crlf:encoding(utf-8)', $filename or die $!;
 
@@ -32,27 +34,29 @@ foreach my $langname (keys %{$data->{td}}) {
 $data = panlex_query_all('/lv', {});
 
 foreach my $lv (@{$data->{result}}) {
-    push @{$td_uid{$lv->{td}}}, $lv->{uid};
+    push @{$td{uid}{$lv->{td}}}, $lv->{uid};
 }
 
 $data = panlex_query_all('/ex', {
-    uid => \@ISO_UID,
+    uid => [ @ISO_UID, $GLOTTO_UID ],
     trtd => \@langnames,
-    include => 'trtd',
+    include => ['trtd','uid'],
 });
 
 foreach my $ex (@{$data->{result}}) {
-    push @{$td_iso{$ex->{trtd}}}, $ex->{tt};
+    my $type = $ex->{uid} eq $GLOTTO_UID ? 'glotto' : 'iso';
+    push @{$td{$type}{$ex->{trtd}}}, $ex->{tt};
 }
 
-print join("\t", 'Name', 'PanLex UIDs', 'ISO 639 codes'), "\n";
+print join("\t", 'Name', 'PanLex UIDs', 'ISO 639 codes', 'Glottocodes'), "\n";
 
 foreach my $langname (@langnames) {
     my $td = $langname_td{$langname};
 
     my @info = ($langname);
-    push @info, join('; ', sort { $a cmp $b } uniq @{$td_uid{$td} || []});
-    push @info, join('; ', sort { $a cmp $b } uniq @{$td_iso{$td} || []});
+    push @info, join('; ', sort { $a cmp $b } uniq @{$td{uid}{$td} || []});
+    push @info, join('; ', sort { $a cmp $b } uniq @{$td{iso}{$td} || []});
+    push @info, join('; ', sort { $a cmp $b } uniq @{$td{glotto}{$td} || []});
 
     print join("\t", @info), "\n";
 }
