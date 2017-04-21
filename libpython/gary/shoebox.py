f24ac7a76eec2b913482b0de9c153e8170af8a0d
Gary Krug <garykpdx145@gmail.com>
2017-04-21 09:18:54 -0700

    updated shoebox parser

--- libpython/gary/shoebox.py
+++ libpython/gary/shoebox.py
@@ -4,32 +4,36 @@ import regex as re
 
 from gary import ignore_parens_list
 
-Record = namedtuple('Record', ['dob', 'eng', 'pos', 'phn'])
-
 @ignore_parens_list
 def split_words(text:str) -> list:
     return re.split('\s*;\s*', text)
+
+def empty_line(text):
+    return len(text.strip()) == 0
 
 
 class ShoeboxParser:
     def __init__(self):
         pass
         
-    def parse(self, infile):
+    def parse(self, filename):
         pattern = re.compile('^\\\\(\w+)\s+(.*)$')
         current_record = []
         started = False
-
-        for line in infile:
-            if len(line.strip()) == 0 and started:
+        
+        with open(filename) as infile:
+            for line in infile:
+                
+                if empty_line(line) and started:
+                    if len(current_record) > 0:
+                        yield current_record
+                    current_record = []
+                
+                match = pattern.search(line)
+                
+                if match:
+                    if not match[1].startswith('_'):
+                        started = True
+                        current_record.append( (match[1],match[2]))
+            if len(current_record) > 0:
                 yield current_record
-                current_record = []
-            
-            match = pattern.search(line)
-            
-            if match:
-                if not match[1].startswith('_'):
-                    started = True
-                    current_record.append( (match[1],match[2]))
-
-        yield current_record
