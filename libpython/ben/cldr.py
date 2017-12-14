b2558456dd367def51769fe0b3062308bdcd81d5
Ben Yang <benayang@gmail.com>
2017-12-13 18:56:26 -0800

    changed cldr to use lxml and xpaths

--- libpython/ben/cldr.py
+++ libpython/ben/cldr.py
@@ -4,12 +4,13 @@
 import json
 from collections import defaultdict
 import os
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from ben.panlex import *
 from ben.IETF_to_panlex import from_IETF
 import regex as re
 from langcodes import Language
 from tqdm import tqdm
+from lxml import etree
 
 lang_list = json.load(open(os.path.dirname(__file__) + '/lang_list.json'))
 lang_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(defaultdict)))
@@ -17,7 +18,7 @@ lang_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(defaultdict)))
 for lang in lang_list:
     lang_dict[lang['language']][lang['script']][lang['territory']][lang['variant']] = lang['_uid']
 
-key_list = ['language', 'script', 'territory', 'variant']
+key_list = {'language', 'script', 'territory', 'variant'}
 def get_uid(lang, lang_dict):
     for key in key_list:
         if key not in lang.keys():
@@ -30,25 +31,29 @@ def get_uid(lang, lang_dict):
     except KeyError:
         return ''
 
-def extract(file_list, tag_name, tag_parent, mn_dict, langs_not_found):
+def extract(file_list, xpath, mn_dict, langs_not_found):
     for file in file_list:
-        soup = BeautifulSoup(open(file), 'lxml')
-        lang = {}
-        for key in key_list:
-            try:
-                lang[key] = soup.identity.find(key)['type']
-            except TypeError:
-                lang[key] = ''
+        # soup = BeautifulSoup(open(file), 'lxml')
+        tree = etree.parse(open(file))
+        lang = {_: '' for _ in key_list}
+        for element in tree.xpath("/ldml/identity/*"):
+            if element.tag in lang:
+                lang[element.tag] = element.get("type")
+        # for key in key_list:
+            # try:
+            #     lang[key] = soup.identity.find(key)['type']
+            # except TypeError:
+            #     lang[key] = ''
         uid = get_uid(lang, lang_dict)
         if uid:
-            for tag in soup(tag_name):
-                tag_type = tag['type']
-                if tag_type not in mn_dict:
-                    mn_dict[tag_type] = Mn()
-                text = tag.text
+            for element in tree.xpath(xpath):
+                element_path = "{}[type='{}'".format(xpath, element.get("type"))
+                if element_path not in mn_dict:
+                    mn_dict[element_path] = Mn()
+                text = element.text
                 if text:
-                    mn_dict[tag_type].dn_list.append(Dn(Ex(text, uid)))
-        elif soup(tag_parent):
+                    mn_dict[element_path].dn_list.append(Dn(Ex(text, uid)))
+        else:
             lang['_uid'] = ''
             langs_not_found.append(lang)
 
